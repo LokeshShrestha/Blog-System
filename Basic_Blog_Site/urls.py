7563a249a1600524b8d05c81d3e160c57e2856c6
LokeshShrestha <lokesh591shrestha@gmail.com>
2025-06-24 22:02:26 +0545

    urls setup login

--- Basic_Blog_Site/urls.py
+++ Basic_Blog_Site/urls.py
@@ -20,6 +20,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("members/",include("django.contrib.auth.urls")),
     path("members/",include("members.urls")),
 ]
